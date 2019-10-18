d0e08504fe1323e5880956287f7f61391d1257cc
wtyper <w.typer@wearevirtua.com>
2019-10-18 08:27:45 +0200

    poprawiony route

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -1,16 +1,23 @@
 <?php
-
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
 class PizzaController extends AbstractController
 {
     /**
      * @Route("/pizza", name="pizza")
      */
     public function index()
+    {
+        return $this->render('pizza/index.html.twig', [
+            'controller_name' => 'PizzaController',
+        ]);
+    }
+
+    /**
+     * @Route("/welcome", name="pizza")
+     */
+    public function welcome()
     {
         return $this->render('pizza/index.html.twig', [
             'controller_name' => 'PizzaController',
