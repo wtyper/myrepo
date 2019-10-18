729c0b9142ba1c7c1dc78617bca354ed84788c53
wtyper <w.typer@wearevirtua.com>
2019-10-18 12:26:20 +0200

    zmiana nazwy zmiennej dt->date

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -5,20 +5,15 @@ use Symfony\Component\Routing\Annotation\Route;
 use \DateTime;
 class WelcomeController extends AbstractController
 {
-
-
     /**
      * @Route("/welcome", name="welcome")
      */
     public function welcome()
-
     {
-        $dt = new DateTime();
-
-
+        $date = new DateTime();
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'WelcomeController',
-            'dt' => $dt,
+            'date' => $date,
         ]);
     }
 }
