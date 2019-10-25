e3fe572291fc80fef25f020af23ce24d5862b85a
wtyper <w.typer@wearevirtua.com>
2019-10-25 13:08:35 +0200

    poprawione ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -15,22 +15,22 @@ class ProductController extends AbstractController
      */
     public function createProduct(): Response
     {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
+//        // you can fetch the EntityManager via $this->getDoctrine()
+//        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+//        $entityManager = $this->getDoctrine()->getManager();
+//
+//        $product = new Product();
+//        $product->setName('Keyboard');
+//        $product->setDescription('Ergonomic and stylish!');
+//        $product->setDate_of_creation(\DateTime::createFromFormat('Y-m-d', "2018-09-09"));
+//        $product->setDate_of_last_modification(\DateTime::createFromFormat('Y-m-d', "2018-09-09"));
+//
+//        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+//        $entityManager->persist($product);
+//
+//        // actually executes the queries (i.e. the INSERT query)
+//        $entityManager->flush();
 
-        $product = new Product();
-        $product->setName('Keyboard');
-        $product->setDescription('Ergonomic and stylish!');
-        $product->setDate_of_creation(\DateTime::createFromFormat('Y-m-d', "2018-09-09"));
-        $product->setDate_of_last_modification(\DateTime::createFromFormat('Y-m-d', "2018-09-09"));
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new product with id '.$product->getId());
+        return new Response('Saved new product with id ');//.$product->getId());
     }
 }
