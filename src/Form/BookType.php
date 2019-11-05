1a4d6e3e4072dd87f276ced1f99092c6fd133d1a
wtyper <w.typer@wearevirtua.com>
2019-11-05 13:41:56 +0100

    crud

---
+++ src/Form/BookType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('author')
+            ->add('genre')
+            ->add('yearOfPublishment')
+            ->add('countryOfPublishment')
+            ->add('availability')
+            ->add('dateOfCreate')
+            ->add('dateOfUpdate')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Book::class,
+        ]);
+    }
+}
