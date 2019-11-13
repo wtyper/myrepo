7fc361f6d8f37ad19f3a7f17380e85e1a1743478
wtyper <w.typer@wearevirtua.com>
2019-11-13 14:13:52 +0100

    templates - not working

--- src/Controller/WishListController.php
+++ src/Controller/WishListController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class WishListController extends AbstractController
+
 {
     /**
      * @Route("/wishlist", name="wish_list")
@@ -16,4 +22,62 @@ class WishListController extends AbstractController
             'controller_name' => 'WishListController',
         ]);
     }
+
+    private const WISH_LIST_NAME = 'WISHLIST';
+
+    private $session;
+    /**
+     * @var array
+     */
+    private $wishList;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+        $this->wishList = $this->session->get(self::WISH_LIST_NAME, []);
+    }
+    /**
+     * @Route("/add/{id}", name="wishlist_add", methods={"POST"})
+     */
+    public function add(Product $product): Response
+    {
+        if (!isset($this->getSessionWishList()[$product->getId()])) {
+                $this->wishList[$product->getId()] = $product->getName();
+                $this->addFlash('success', $product->getName() . ' ' . $this->trans('wishlist.added'));
+                $this->setSessionWishList();
+            }
+        return $this->showProducts();
+    }
+    public function setSessionWishList(): void
+    {
+        $this->session->set(self::WISH_LIST_NAME, $this->wishList);
+    }
+    /**
+     * @return Response
+     */
+    private function showProducts(): Response
+    {
+        return $this->redirectToRoute('product_index');
+    }
+
+    /**
+     * @return Response
+     */
+    public function dropdown(): Response
+    {
+        return $this->render('wishlist/_dropdown.html.twig', [
+            'wishList' => $this->getSessionWishList()
+        ]);
+    }
+    /**
+     * @param Product $product
+     * @return Response
+     */
+    public function showWishListProductForm(Product $product): Response
+    {
+        return $this->render(!isset($this->getSessionWishList()[$product->getId()]));
+    }
+
+
+
 }
