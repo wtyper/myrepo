0195532a2fc1bf7ccf29b16449147d97ac428baa
wtyper <w.typer@wearevirtua.com>
2019-11-15 15:53:04 +0100

    popraw dodawanie

--- src/Controller/WishListController.php
+++ src/Controller/WishListController.php
@@ -1,92 +1,30 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Attribute\NamespacedAttributeBag;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
 use Symfony\Component\Routing\Annotation\Route;
-
-
-class WishListController extends AbstractController
-
+/**
+ * @Route("/wishlist")
+ */
+class WishlistController extends AbstractController
 {
     /**
-     * @Route("/wishlist", name="wishlist")
-     *
+     * @Route("/add/{id}", name ="wishlist_add")
      */
-    public function index()
+    public function add(Product $product)
     {
-        return $this->render('wishlist/index.html.twig', [
-            'controller_name' => 'WishListController',
-        ]);
-    }
-
-    private const WISH_LIST = 'WISHLIST';
-
-    private $session;
-
-    /**
-     * @var array
-     */
-    private $wishList;
-
-    public function __construct(SessionInterface $session)
-    {
-        $this->session = $session;
-        $this->wishList = $this->session->get(self::WISH_LIST, []);
-    }
-
-    /**
-     * @Route("/add/{id}", name="wishlist_add", methods={"POST"})
-     */
-    public function add(Product $product): Response
-    {
-        if (!isset($this->getSessionWishList()[$product->getId()])) {
-                $this->wishList[$product->getId()] = $product->getName();
-                $this->addFlash('Product added to wishlist!', $product->getName());
-                $this->setSessionWishList();
-            }
-        return $this->showProducts();
-    }
-
-    public function setSessionWishList(): void
-    {
-        $this->session->set(self::WISH_LIST, $this->wishList);
-    }
-
-    /**
-     * @return Response
-     */
-    private function showProducts(): Response
-    {
+        $session = new Session(new NativeSessionStorage(), new NamespacedAttributeBag());
+        $wishlist = $session->all();
+        $id = $product->getId();
+        if(!isset($wishlist['wishlist'])) {
+            $session->set('wishlist/' . $id,$id);
+            $this->addFlash('notice', 'Add to wishlist ID:' . $id);
+        }
         return $this->redirectToRoute('product_index');
-    }
-
-    /**
-     * @return array
-     */
-    public function getSessionWishList(): array
-    {
-        return $this->session->get(self::WISH_LIST, []);
-    }
-
-    /**
-     * @return Response
-     */
-    public function dropdown(): Response
-    {
-        return $this->render('wishlist/_dropdown.html.twig', [
-            'wishList' => $this->getSessionWishList()
-        ]);
-    }
-
-    public function showWishListProductForm(Product $product): Response
-    {
-        return $this->render(!isset($this->getSessionWishList()[$product->getId()])
-            ? 'wishlist/_add_form.html.twig' : [
-            'product' => $product
-        ]);
     }
 }
